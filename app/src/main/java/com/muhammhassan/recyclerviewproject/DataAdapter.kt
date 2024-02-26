5ee97858fa684788030136bd5f2af39e5918e632
Muhammad Hassan Thalib <muhammadhassan7788@gmail.com>
2024-02-26 20:35:23 +0700

    Add Comment

--- app/src/main/java/com/muhammhassan/recyclerviewproject/DataAdapter.kt
+++ app/src/main/java/com/muhammhassan/recyclerviewproject/DataAdapter.kt
@@ -8,16 +8,19 @@ import com.muhammhassan.recyclerviewproject.databinding.ItemListLayoutBinding
 
 class DataAdapter(val list: List<Data>) : RecyclerView.Adapter<DataAdapter.DataViewHolder>() {
 
+    //mendefinisikan layout yang akan digunakan
     inner class DataViewHolder(val binding: ItemListLayoutBinding) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
+        //menetapkan layout yang digunakan pada setiap item yang ditampilkan
         val binding =
             ItemListLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return DataViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
+        //Mengembalikan jumlah data yang ditampilkan
         return list.size
     }
 
@@ -25,10 +28,12 @@ class DataAdapter(val list: List<Data>) : RecyclerView.Adapter<DataAdapter.DataV
         val item = list[position]
         with(holder) {
             binding.apply {
+                //Menentukan nilai dari setiap komponen yang ditampilkan pada item
                 tvJudul.text = item.judul
                 tvDesc.text = item.desc
             }
 
+            //Menambahkan aksi pada saat item ditekan
             itemView.setOnClickListener {
                 Toast.makeText(itemView.context, item.judul + "Ditekan", Toast.LENGTH_SHORT).show()
             }
